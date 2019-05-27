45731cfff2f33f1c22864a1858db3594f3e81ef5
unknown <matheusprado55@gmail.com>
2019-05-26 21:49:34 -0300

    Fim

--- TopicosEspecial/Program.cs
+++ TopicosEspecial/Program.cs
@@ -190,7 +190,7 @@ namespace TopicosEspecial
 
             //TimeSpan teste = new TimeSpan(0, 1, 30);
             //Console.WriteLine(teste);
-            //Console.WriteLine(teste.Ticks); //Duvida------------------
+            //Console.WriteLine(teste.Ticks); // Ticks (100 NanoSegundo);
 
             //Console.WriteLine("------------------");
 
@@ -301,20 +301,49 @@ namespace TopicosEspecial
 
             //DateTimeKind------------------------
 
-            DateTime d1 = new DateTime(2000, 8, 15, 13, 5, 58, DateTimeKind.Local);
-            DateTime d2 = new DateTime(2000, 8, 15, 13, 5, 58, DateTimeKind.Utc);
-            DateTime d3 = new DateTime(2000, 8, 15, 13, 5, 58);
+            //DateTime d1 = new DateTime(2000, 8, 15, 13, 5, 58, DateTimeKind.Local);
+            //DateTime d2 = new DateTime(2000, 8, 15, 13, 5, 58, DateTimeKind.Utc);
+            //DateTime d3 = new DateTime(2000, 8, 15, 13, 5, 58);
 
 
-            Console.WriteLine("d1: " + d1);
+            //Console.WriteLine("d1: " + d1);
 
-            Console.WriteLine("d1 Kind: " + d1.Kind);
+            //Console.WriteLine("d1 Kind: " + d1.Kind);
+
+            //Console.WriteLine("d1 to Local: " + d1.ToLocalTime());
+
+            //Console.WriteLine("d1 to Utc: " + d1.ToUniversalTime());
+
+            //Console.WriteLine(); Console.WriteLine("d2: " + d2);
+
+            //Console.WriteLine("d2 Kind: " + d2.Kind);
+
+            //Console.WriteLine("d2 to Local: " + d2.ToLocalTime());
+
+            //Console.WriteLine("d2 to Utc: " + d2.ToUniversalTime());
+
+            //Console.WriteLine(); Console.WriteLine("d3: " + d3);
+
+            //Console.WriteLine("d3 Kind: " + d3.Kind);
+
+            //Console.WriteLine("d3 to Local: " + d3.ToLocalTime());
+
+            //Console.WriteLine("d3 to Utc: " + d3.ToUniversalTime());
+
+            //demo---
+            DateTime d1 = DateTime.Parse("2000-08-15 13:05:58");
+            DateTime d2 = DateTime.Parse("2000-08-15T13:05:58Z"); // cria local DateTime
+
+
+            Console.WriteLine("d1: " + d1); Console.WriteLine("d1 Kind: " + d1.Kind);
 
             Console.WriteLine("d1 to Local: " + d1.ToLocalTime());
 
             Console.WriteLine("d1 to Utc: " + d1.ToUniversalTime());
 
-            Console.WriteLine(); Console.WriteLine("d2: " + d2);
+            Console.WriteLine();
+
+            Console.WriteLine("d2: " + d2);
 
             Console.WriteLine("d2 Kind: " + d2.Kind);
 
@@ -322,13 +351,11 @@ namespace TopicosEspecial
 
             Console.WriteLine("d2 to Utc: " + d2.ToUniversalTime());
 
-            Console.WriteLine(); Console.WriteLine("d3: " + d3);
+            Console.WriteLine();
 
-            Console.WriteLine("d3 Kind: " + d3.Kind);
+            Console.WriteLine(d2.ToString("yyyy-MM-ddTHH:mm:ssZ"));
 
-            Console.WriteLine("d3 to Local: " + d3.ToLocalTime());
-
-            Console.WriteLine("d3 to Utc: " + d3.ToUniversalTime());
+            // cuidado! Console.WriteLine(d2.ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ssZ"));
         }
     }
 }
